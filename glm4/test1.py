29d56fea8ff4379af847c42abb1f118245d3f980
frankfangy <frankfangy@gmail.com>
2024-12-07 21:31:05 +0800

    '..'

--- glm4/test1.py
+++ glm4/test1.py
@@ -1,16 +1,10 @@
 from langchain_community.chat_models import ChatZhipuAI
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
-
-# 设置API密钥
-import os
-os.environ["ZHIPUAI_API_KEY"] = os.environ["ZP_KEY"]  # 填入你自己的API Key 
-print( os.environ["ZHIPUAI_API_KEY"] )
+ 
 # 初始化GLM-4模型
 chat = ChatZhipuAI(
     model="glm-4",
-    temperature=0.5,
-    zhipuai_api_key= os.environ["ZP_KEY"] , 
-    zhipuai_api_base= 'https://open.bigmodel.cn/api/paas/v4/chat/completions'
+    temperature=0.5 
   )
 
 # 构建消息对象
